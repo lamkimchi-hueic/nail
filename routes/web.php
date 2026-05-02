04bd147124a358bd48983d9745f5e68b12d434a3
lkc24th <nguyentdmy@students.hueic.edu.vn>
2026-05-02 22:40:47 +0700

    Test: Add health-check to web.php

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,10 @@ Route::get('/test-route', function() {
     return "WEB ROUTE WORKING";
 });
 
+Route::get('/api/health-check', function () {
+    return response()->json(['status' => 'ok', 'message' => 'API WORKING FROM WEB.PHP']);
+});
+
 // Route::get('/{any}', function () {
 //     return view('welcome');
 // })->where('any', '^(?!api).*$');
